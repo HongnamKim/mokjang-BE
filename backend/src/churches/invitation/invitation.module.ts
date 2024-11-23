e7d43f86fd075d75c46e78c9b5065684d3adc2c7
HongnamKim <ghdskawkd@naver.com>
2024-11-24 06:32:59 +0900

    신도 CRUD

--- backend/src/churches/invitation/invitation.module.ts
+++ backend/src/churches/invitation/invitation.module.ts
@@ -3,13 +3,15 @@ import { InvitationService } from './invitation.service';
 import { InvitationController } from './invitation.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvitationModel } from './entity/invitation.entity';
-import { BelieverModel } from './entity/believer.entity';
+import { BelieverModel } from '../believers/entity/believer.entity';
 import { ChurchesModule } from '../churches.module';
+import { BelieversModule } from '../believers/believers.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([InvitationModel, BelieverModel]),
     ChurchesModule,
+    BelieversModule,
   ],
   controllers: [InvitationController],
   providers: [InvitationService],
