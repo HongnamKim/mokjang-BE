8fe24a9d61689fc5ec2c272cd970cb4824e78138
HongnamKim <ghdskawkd@naver.com>
2025-02-03 22:10:31 +0900

    refactor: Auth 시스템 구조 개선 및 기능 강화
    # 주요 내용
    - 회원 관리 로직 분리
    - 인증 관련 엔드포인트 재구성
    - 회원가입 응답 개선
    
    # 세부 내용
    ## 모듈 구조 개선
    - AuthModule에서 회원 관리 로직 분리
     - UserService: 정회원 관리
     - TempUserService: 임시회원 관리
    - 유저 관련 엔드포인트 UserController로 분리
    
    ## 회원가입 프로세스 개선
    - 회원가입 완료 시 토큰 발급
     - accessToken 발급 추가
     - refreshToken 발급 추가

--- backend/src/auth/service/user.service.ts
+++ backend/src/auth/service/user.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserModel } from '../entity/user.entity';
 import { QueryRunner, Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/user/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -12,6 +17,51 @@ export class UserService {
 
   private getUserRepository(qr?: QueryRunner) {
     return qr ? qr.manager.getRepository(UserModel) : this.userRepository;
+  }
+
+  async isExistUser(provider: string, providerId: string, qr?: QueryRunner) {
+    const userRepository = this.getUserRepository(qr);
+
+    const user = await userRepository.findOne({
+      where: {
+        provider,
+        providerId,
+      },
+    });
+
+    return !!user;
+  }
+
+  findUserModelByOAuth(provider: string, providerId: string, qr?: QueryRunner) {
+    const userRepository = this.getUserRepository(qr);
+
+    return userRepository.findOne({
+      where: {
+        provider,
+        providerId,
+      },
+    });
+  }
+
+  async getUserModelByProvider(
+    provider: string,
+    providerId: string,
+    qr?: QueryRunner,
+  ) {
+    const userRepository = this.getUserRepository(qr);
+
+    const user = await userRepository.findOne({
+      where: {
+        provider,
+        providerId,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('존재하지 않는 유저입니다.');
+    }
+
+    return user;
   }
 
   getUserById(id: number) {
@@ -25,6 +75,20 @@ export class UserService {
         adminChurch: true,
         managingChurch: true,
       },
+    });
+  }
+
+  async createUser(dto: CreateUserDto, qr?: QueryRunner) {
+    const isExistUser = await this.isExistUser(dto.provider, dto.providerId);
+
+    if (isExistUser) {
+      throw new BadRequestException('이미 가입 이력이 있는 소셜 계정입니다.');
+    }
+
+    const userRepository = this.getUserRepository(qr);
+
+    return userRepository.save({
+      ...dto,
     });
   }
 }
