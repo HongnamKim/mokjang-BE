ff8acf60df079030cd5cc3a9662e3067f37b4d80
HongnamKim <ghdskawkd@naver.com>
2025-02-06 10:24:28 +0900

    docs: 교인 가족 관련 API 문서화
    # 주요 내용
    - 기존 가족 불러오기 API Deprecated 처리
    - 교인 가족 관련 API 문서 작성
    
    # 세부 내용
    ## API 상태 변경
    - 기존 가족 불러오기 엔드포인트 Deprecated 표시
    
    ## Swagger 문서 추가
    - 가족 관련 API 엔드포인트 설명

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -38,26 +38,31 @@ export class AppController {
     return this.dummyDataService.createRandomMembers(churchId, 20);
   }
 
+  @ApiOperation({ summary: 'NotFoundException' })
   @Get('error/not-found')
   getNotFoundError() {
     throw new NotFoundException('해당 정보를 찾을 수 없습니다.');
   }
 
+  @ApiOperation({ summary: 'BadRequestException' })
   @Get('error/bad-request')
   getBadRequest() {
     throw new BadRequestException('잘못된 요청입니다.');
   }
 
+  @ApiOperation({ summary: 'UnauthorizedException' })
   @Get('error/unauthorized')
   getUnauthorizedError() {
     throw new UnauthorizedException('회원 인증에 실패하였습니다.');
   }
 
+  @ApiOperation({ summary: 'ForbiddenException' })
   @Get('error/forbidden')
   getForbiddenError() {
     throw new ForbiddenException('권한이 없습니다.');
   }
 
+  @ApiOperation({ summary: 'InternalServerErrorException' })
   @Get('error/internal-server')
   getInternServerError() {
     throw new InternalServerErrorException('데이터 저장 실패');
