25ece03c163cfaa181008638b93a8d1fcac4988e
HongnamKim <ghdskawkd@naver.com>
2025-11-29 15:16:55 +0900

    fix: 에러메시지 오탈자 수정

--- backend/src/members/dto/request/create-bulk-member.dto.ts
+++ backend/src/members/dto/request/create-bulk-member.dto.ts
@@ -22,7 +22,7 @@ export class BulkMemberDto {
     default: new Date().toISOString().slice(0, 10),
   })
   @IsDateString({ strict: true })
-  @IsYYYYMMDD('등록일쟈')
+  @IsYYYYMMDD('등록일자')
   @IsOptional()
   등록일자?: string; //"2025-09-22",
 
